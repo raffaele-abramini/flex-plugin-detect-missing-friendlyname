1891d032f07ded2a8bde2cb7a61f86ba318efb5b
Raffaele Abramini <rabramini@twilio.com>
2021-05-20 15:11:41 +0100

    Add try-catch to avoid any impact on the agent experience if something fails

--- src/DetectMissingFriendlynamePlugin.js
+++ src/DetectMissingFriendlynamePlugin.js
@@ -1,21 +1,13 @@
-import React from 'react';
-import { VERSION } from '@twilio/flex-ui';
-import { FlexPlugin } from 'flex-plugin';
+import React from "react";
+import { FlexPlugin } from "flex-plugin";
 
-const PLUGIN_NAME = 'DetectMissingFriendlynamePlugin';
+const PLUGIN_NAME = "DetectMissingFriendlynamePlugin";
 
 export default class DetectMissingFriendlynamePlugin extends FlexPlugin {
   constructor() {
     super(PLUGIN_NAME);
   }
 
-  /**
-   * This code is run when your plugin is being started
-   * Use this to modify any UI components or attach to the actions framework
-   *
-   * @param flex { typeof import('@twilio/flex-ui') }
-   * @param manager { import('@twilio/flex-ui').Manager }
-   */
   init(flex, manager) {
     const MissingFriendlyName = {};
 
@@ -26,38 +18,43 @@ export default class DetectMissingFriendlynamePlugin extends FlexPlugin {
       }
 
       render() {
-       try {
-         const {channel} = this.props;
+        try {
+          const { channel } = this.props;
 
-         // if there's not channel source, or there are not members or we already analyzed this channel, don't do anything
-         if (!channel?.source ||!channel?.members?.size || MissingFriendlyName[channel?.source.sid]) {
-           return <></>;
-         }
+          // if there's not channel source, or there are not members or we already analyzed this channel, don't do anything
+          if (!channel?.source || !channel?.members?.size || MissingFriendlyName[channel?.source.sid]) {
+            return <></>;
+          }
 
-         const membersWithoutFriendlyNames = Array.from(channel.members.values()).filter(m => !m.friendlyName);
-         const usersFromStore = manager.store.getState().flex.chat.users;
+          const membersWithoutFriendlyNames = Array.from(channel.members.values()).filter(m => !m.friendlyName);
 
-         if (membersWithoutFriendlyNames.length) {
-           MissingFriendlyName[channel.source.sid] = {
-             channelMembers: membersWithoutFriendlyNames,
-             chatUsers: membersWithoutFriendlyNames.map(m => usersFromStore[m.source.identity])
-           }
+          // if there are members without friendlyNames
+          if (membersWithoutFriendlyNames.length) {
+            const usersFromStore = manager.store.getState().flex.chat.users;
 
-           // Before 1.26
-           flex.ErrorManager.createAndProcessError(`Detected channel users without a friendly name for ${channel.source.sid}`, {
-             context: "Custom",
-             description:`Detected channel users without a friendly name for ${channel.source.sid}.
+            MissingFriendlyName[channel.source.sid] = {
+              channelMembers: membersWithoutFriendlyNames,
+              chatUsers: membersWithoutFriendlyNames.map(m => usersFromStore[m.source.identity]),
+            };
+
+            // Notice, this approach will get deprecate it 1.26
+            flex.ErrorManager.createAndProcessError(`Detected channel users without a friendly name for ${channel.source.sid}`, {
+              context: "Custom",
+              description: `Detected channel users without a friendly name for ${channel.source.sid}.
 Participant: ${membersWithoutFriendlyNames.map(m => `${m.source.identity} - ${m.source.sid}`).join(",")}
 Member: ${membersWithoutFriendlyNames.map(m => `${usersFromStore[m.source.identity]?.identity} - ${usersFromStore[m.source.identity]?.descriptor?.sid}`).join(",")}
-`
-           })
+`,
+            });
 
-           return <></>;
-         }
-       } catch(e) {}
+            return <></>;
+          }
+        } catch (e) {
+          console.warn("Something went wrong with the MissingFriendlyName plugin render logic", e);
+          return <></>;
+        }
       };
     }
 
-    flex.MessagingCanvas.Content.add(<SpyComponent key="Spy" />);
+    flex.MessagingCanvas.Content.add(<SpyComponent key="Spy"/>);
   }
 }
