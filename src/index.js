7d1bd5e88818017e08d714261caded5a5052dc2a
Raffaele Abramini <rabramini@twilio.com>
2021-05-20 13:04:10 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import * as FlexPlugin from 'flex-plugin';
+import DetectMissingFriendlynamePlugin from './DetectMissingFriendlynamePlugin';
+
+FlexPlugin.loadPlugin(DetectMissingFriendlynamePlugin);
